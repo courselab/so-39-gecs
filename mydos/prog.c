dc4bd35d9b1556092360200a968ffe3f0ddfdf55
Samuel Veronez <sveronez@usp.br>
2025-07-08 17:48:48 -0300

    mydos

---
+++ mydos/prog.c
@@ -0,0 +1,17 @@
+/*
+ *    SPDX-FileCopyrightText: 2025 Samuel Veronez <sveronez@usp.br>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  Samuel Veronez <sveronez@usp.br>
+ */
+
+#include "tydos.h"
+
+int main()
+{
+  puts("Hello World\n");
+  return 0;
+}
